c70765711f1066936e3c3aa45868b7bc2dab9dd3
Simone Pavone <simonebest56@gmail.com>
2024-04-16 17:48:46 +0200

    secondo commit - add new screen for the cart_screen

--- lib/presentation/controllers/cart/cart_controller.dart
+++ lib/presentation/controllers/cart/cart_controller.dart
@@ -5,7 +5,17 @@ class CartController extends GetxController {
   final RxList<MenuItem> cartItems = <MenuItem>[].obs;
 
   void addItemToCart(MenuItem item) {
-    cartItems.add(item);
+    // Cerca l'articolo nel carrello
+    final existingItemIndex = cartItems.indexWhere((cartItem) => cartItem.id == item.id);
+
+    // Se l'articolo è già nel carrello, incrementa la quantità
+    if (existingItemIndex != -1) {
+      cartItems[existingItemIndex].quantity++;
+    } else {
+      // Altrimenti aggiungi l'articolo al carrello con quantità 1
+      item.quantity = 1;
+      cartItems.add(item);
+    }
   }
 
   void removeFromCart(MenuItem item) {
@@ -15,7 +25,7 @@ class CartController extends GetxController {
   double getTotalPrice() {
     double total = 0.0;
     for (var item in cartItems) {
-      total += item.price;
+      total += item.price * item.quantity; // Moltiplica il prezzo per la quantità
     }
     return total;
   }
